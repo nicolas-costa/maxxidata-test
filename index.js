784881aa889b28586b3da60225dfdba7b307beb3
Nícolas Costa <nicolas.nico@hotmail.com>
2021-03-14 17:45:05 -0300

    Adicionado jest e scripts para rodar os testes. Atualizado docker-compose para configurar corretamente o container que roda o banco de testes.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import consign from 'consign';
-import bodyParser from 'body-parser';
+import express from "express";
+import consign from "consign";
+import bodyParser from "body-parser";
 const app = express();
-require('dotenv').config({
-    path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env"
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env",
 });
 
 const jsonParser = bodyParser.json();
@@ -13,13 +13,14 @@ consign({ verbose: false }, { cwd: "src" })
   .include("./src/models/index.js")
   .then("./src/controllers/")
   .then("./src/routes/")
-    .then('./src/services/')
+  .then("./src/services/")
+  .then("./src/routes/validators/")
   .into(app);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log('listening on', port);
+  console.log("listening on", port);
 });
 
 export default app;
