dbf31fcabfb7d3f534ce171ba66ee82f3e809799
Nícolas Costa <nicolas.nico@hotmail.com>
2021-03-13 21:02:11 -0300

    Adicionado cosign e scripts de debug.

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import consign from 'consign';
+import bodyParser from 'body-parser';
+const app = express();
+require('dotenv').config({
+    path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env"
+});
+
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
+
+consign({ verbose: false }, { cwd: "src" })
+  .include("./src/models/index.js")
+  .then("./src/controllers/")
+  .then("./src/routes/")
+    .then('./src/services/')
+  .into(app);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('listening on', port);
+});
+
+export default app;
